35b2ddb31eff73ffd2f99aab5bc2c79b5fe62a4d
Michael Lynn (RPi1) <micllynn@gmail.com>
2020-07-22 16:12:48 -0400

    IR pins changed -M

--- calibrate_syringe.py
+++ calibrate_syringe.py
@@ -4,4 +4,4 @@ rstep = RewardStepper('rstep', pin_motor_off=27, pin_step=18,
                       pin_dir=17, pin_not_at_lim=14,
                       rate=200, volume=4, t_start=1)
 
-rstep.calibrate(n_steps=10000)
+rstep.calibrate(n_steps=30000)
